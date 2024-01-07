b67039e4552c5e53a7a06d900d31645b8fb62d16
Ritik Raj Gupta <150698330+ritikrajg@users.noreply.github.com>
2024-01-07 23:48:30 +0530

    Time: 3 ms (94.27%), Space: 6.3 MB (39.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool check(vector<int> &arr,int s,int e){
         if(s>e){
@@ -23,5 +23,28 @@ public:
             x=x/10;
         }
         return check(a,0,a.size()-1);
+    }
+};*/
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int n=x;
+        long long rev=0;
+        if(x<0){
+            return false;
+        }
+        if(x==0){
+            return true;
+        }
+        while(n!=0){
+            rev=rev*10+n%10;
+            n=n/10;
+        }
+        if(x==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
